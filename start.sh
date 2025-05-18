123c014ce724d3dd3282dcdca9202f866985c996
nlabrazi <na.labrazi@gmail.com>
2025-05-18 23:13:33 +0200

    chore: update screenshot and start (2025-05-18 23:13)

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ echo "Starting bot with mode: $RUN_MODE"
 if [ "$RUN_MODE" = "bot" ]; then
   python main.py
 elif [ "$RUN_MODE" = "clean" ]; then
-  python clean_bot_data.py
+  python utils/clean_bot_data.py
 else
   echo "Unknown RUN_MODE: $RUN_MODE"
   exit 1
